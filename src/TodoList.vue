09ece75ba788d7112ecfa5ce6a0883660d1d1568
Aron Simon <aronnorbert2001@gmail.com>
2023-08-08 09:38:40 +0300

    create-add-to-do
    Created an add to-do button as well as input.
    Furthermore, I created an AddToDo method

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -1,11 +1,27 @@
 <template>
-    <form></form>
+    <form>
+        <input type="text" v-model="todo">
+        <button @click.prevent="addToDo">Submit</button>
+    </form>
+    <ul v-for="item in todos">
+        <li>{{ item }}<button>Delete</button></li>
+    </ul>
 </template>
 
-<script lang="ts">
-export default {
+<script setup lang="ts">
+import { ref } from 'vue';
+        const todo: string = ref("")
+        const todos: string[] = ref([]) 
 
-}
+        function addToDo() {
+            if (todo.value) {
+                if (!todos.value.includes(todo.value)) {
+                    todos.value.push(todo.value)
+                }
+                todo.value = ""
+            }
+        }
+
 </script>
 
 <style scoped>
