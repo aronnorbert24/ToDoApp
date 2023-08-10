820ea60208cf824301e26427fe1c8c53e3392f0c
Aron Simon <aronnorbert2001@gmail.com>
2023-08-10 09:49:14 +0300

    ITL-9 Add an add button, include a icon library or include the icon from the design + installed tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        header: ['Neue Haas Grotesk Display Pro', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
