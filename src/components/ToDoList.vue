75f20f8c5223e6cc74a24547afb9abadb0e723ce
Aron Simon <aronnorbert2001@gmail.com>
2023-08-10 16:19:18 +0300

    ITL-11 Create the Placeholder img when the user is in the home page and no todos added to the todo list

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,9 +2,11 @@
   <ul v-for="item in todos" :key="item">
     <li>{{ item }}<button @click="emit('deleteToDo', item)">Delete</button></li>
   </ul>
+  <EmptyListImage v-if="!todos.length" />
 </template>
 
 <script setup lang="ts">
+  import EmptyListImage from './EmptyListImage.vue'
   defineProps(['todos'])
 
   const emit = defineEmits(['deleteToDo'])
