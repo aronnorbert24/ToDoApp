3dfde1e5fa810d57e47f4cb854207a85c02ec9e9
Aron Simon <aronnorbert2001@gmail.com>
2023-08-25 10:05:57 +0300

    ITL-23 Made changes according to the review

--- src/components/ToDoSearch.vue
+++ src/components/ToDoSearch.vue
@@ -10,8 +10,8 @@
       class="ml-4 mr-auto flex h-6 w-32 items-center justify-center indent-1 font-header text-lg font-semibold leading-6 text-gray-600 phone:w-20 phone:font-priority phone:text-sm phone:font-medium phone:tracking-tighter phone:text-slate-950"
     />
     <button
-      @click="submitSearch"
       class="w-18 mr-6 flex h-8 items-center justify-between rounded-lg bg-black text-center font-header text-sm font-semibold leading-4 text-white phone:mr-4 phone:w-16 phone:pr-14 phone:text-xs phone:leading-3"
+      @click="submitSearch"
     >
       Search
     </button>
