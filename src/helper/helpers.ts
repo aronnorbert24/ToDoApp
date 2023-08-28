a019834eff5d2f6d4dba223003c60316385076f5
Aron Simon <aronnorbert2001@gmail.com>
2023-08-28 10:13:55 +0300

    ITL-23 Made changes regarding the reviews

--- src/helper/helpers.ts
+++ src/helper/helpers.ts
@@ -10,16 +10,15 @@ export function formatDate(formattingDate: Date, comp: string) {
   }
 }
 
-const debounce = (fn: Function, duration: number) => {
-  let timer: number
-  return (...args: []) => {
-    if (timer) {
-      clearTimeout(timer)
-    }
+export function debounce<Params extends any[]>(
+  func: (...args: Params) => any,
+  timeout: number
+): (...args: Params) => void {
+  let timer: ReturnType<typeof setTimeout>
+  return (...args: Params) => {
+    clearTimeout(timer)
     timer = setTimeout(() => {
-      fn(...args)
-    }, duration)
+      func(...args)
+    }, timeout)
   }
 }
-
-export default debounce
