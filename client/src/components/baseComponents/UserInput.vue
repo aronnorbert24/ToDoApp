63c2dcb81ca3c8592720e0a694cf872138e73d08
Aron Simon <aronnorbert2001@gmail.com>
2023-09-07 10:28:51 +0300

    ITL-34 Create a Login Component

--- client/src/components/baseComponents/UserInput.vue
+++ client/src/components/baseComponents/UserInput.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="mt-6 flex justify-between px-2">
+  <div class="ml-auto mr-auto mt-6 flex justify-between px-40 phone:w-fit phone:px-4">
     <label :for="property">{{ label }}</label>
-    <input :type="property" :name="property" :id="property" class="rounded-lg bg-slate-100 indent-2 outline-black" />
+    <input
+      v-model="value"
+      :type="property"
+      :name="property"
+      :id="property"
+      class="rounded-lg bg-slate-100 indent-2 outline-black phone:w-7/12"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-  interface Props {
+import { computed } from 'vue'
+
+interface Props {
   property: string
   label: string
+  modelValue: string
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', email: string, name: string): void
+}>()
+
+const value = computed({
+  get: () => props.modelValue,
+  set: (newValue) => emit('update:modelValue', newValue, props.label),
+})
 </script>
