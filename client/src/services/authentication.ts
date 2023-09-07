a563aee64202cf8e74b764af53bc8efd30eac306
Aron Simon <aronnorbert2001@gmail.com>
2023-09-07 10:30:56 +0300

    ITL-36 Create an API connection to the server from frontend

--- client/src/services/authentication.ts
+++ client/src/services/authentication.ts
@@ -3,24 +3,31 @@ import { User } from '../types/user'
 
 export async function loginUser(email: string, password: string) {
   try {
-    await axios.post('/login', {
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/login`, {
       email,
       password,
     })
+
+    localStorage.setItem('firstName', response.data.firstName)
+    localStorage.setItem('lastName', response.data.lastName)
+    return response.data
   } catch (error) {
     console.error(error)
+    throw error
   }
 }
 
 export async function registerUser(data: User) {
   try {
-    await axios.post('/register-user', {
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/register-user`, {
       firstName: data.firstName,
       lastName: data.lastName,
       email: data.email,
       password: data.password,
     })
+    return response.data
   } catch (error) {
     console.error(error)
+    throw error
   }
 }
