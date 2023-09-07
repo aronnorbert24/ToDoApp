7b6c9623592bab99cd800f5ef6e3d0e8e5ae381e
Aron Simon <aronnorbert2001@gmail.com>
2023-09-07 10:29:22 +0300

    ITL-35 Create a Register Component

--- client/src/views/RegisterPage.vue
+++ client/src/views/RegisterPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen w-screen flex-col bg-gradient-to-br from-cyan-400 to-green-500 pt-10 font-header">
     <div class="ml-auto mr-auto h-1/6 w-1/2 items-center justify-center rounded-3xl bg-yellow-50">
-      <p class="pt-8 text-4xl font-semibold leading-10">Welcome to your To-do App</p>
+      <p class="pt-8 text-4xl font-semibold leading-10 phone:pt-10 phone:text-xl">Welcome to your To-do App</p>
     </div>
     <div class="h-2/55 m-auto mt-10 justify-center rounded-3xl bg-yellow-50 phone:w-8/12 computer:w-4/12">
       <p class="ml-auto mr-auto w-full pt-4 text-3xl font-semibold">Register</p>
@@ -11,8 +11,15 @@
       <UserInput :label="'Email:'" :property="'email'" v-model="user.email" />
       <UserInput :label="'Password:'" :property="'password'" v-model="user.password" />
 
+      <div
+        v-if="errorMessage"
+        class="mx-2 mt-4 rounded-lg bg-red-300 text-center font-header text-sm font-semibold text-red-600"
+      >
+        {{ errorMessage }}
+      </div>
+
       <button
-        class="mt-10 h-10 w-10/12 rounded-3xl border-none bg-gradient-to-br from-cyan-400 to-green-500 font-semibold text-white transition-transform duration-500 ease-in-out hover:scale-110"
+        class="mt-10 h-10 w-6/12 rounded-3xl border-none bg-gradient-to-br from-cyan-400 to-green-500 font-semibold text-white transition-transform duration-500 ease-in-out hover:scale-110 phone:mt-5"
         @click="register"
       >
         Register
@@ -43,7 +50,12 @@ const errorMessage = ref('')
 const router = useRouter()
 
 async function register() {
+  if (!isInputValid()) {
+    return
+  }
+
   try {
+    console.log(user.value)
     await registerUser(user.value)
   } catch (error) {
     console.error('Register Error', error)
@@ -52,5 +64,42 @@ async function register() {
 
   await loginUser(user.value.email, user.value.password)
   router.push({ name: 'Dashboard' })
+}
+
+function isInputValid() {
+  const isAlphabetical = /^[a-zA-Z]+$/
+  const isFirstLetterCapital = /^[A-Z].*/
+
+  if (
+    user.value.firstName.trim() === '' ||
+    user.value.lastName.trim() === '' ||
+    user.value.email.trim() === '' ||
+    user.value.password.trim() === ''
+  ) {
+    errorMessage.value = 'Please fill in every field.'
+    return false
+  }
+
+  if (!isAlphabetical.test(user.value.firstName) || !isAlphabetical.test(user.value.lastName)) {
+    errorMessage.value = 'Please enter a valid name'
+    return false
+  }
+
+  if (!isFirstLetterCapital.test(user.value.firstName) || !isFirstLetterCapital.test(user.value.lastName)) {
+    errorMessage.value = 'First and Last names should start with an uppercase letter.'
+    return false
+  }
+
+  if (!/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(user.value.email)) {
+    errorMessage.value = 'Please enter a valid email address'
+    return false
+  }
+
+  if (user.value.password.length < 8) {
+    errorMessage.value = 'Password must be at least 8 characters long.'
+    return false
+  }
+
+  return true
 }
 </script>
