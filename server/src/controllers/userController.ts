c0b973fba194d6f558c948cf5563f1defd292d33
Aron Simon <aronnorbert2001@gmail.com>
2023-09-07 10:36:01 +0300

    ITL-38 Create a User Controller file

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,44 @@
-import { Request, Response } from "express";
-import userService from "../services/userService";
+import { Request, Response } from 'express'
+import userService from '../services/userService'
 
-exports.register = async function (req: Request, res: Response) {
-	try {
-		const user = await userService.register(req.body);
-	} catch {}
-};
+class UserController {
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body
+
+      const foundUser = await userService.findUserByEmail(email)
+
+      if (!foundUser) {
+        return res.sendStatus(409)
+      }
+
+      if (password === foundUser.password) {
+        res.status(200).json(foundUser)
+      } else {
+        res.sendStatus(401)
+      }
+    } catch (error) {
+      console.error(error)
+      return res.sendStatus(400)
+    }
+  }
+  async register(req: Request, res: Response) {
+    try {
+      const newUser = req.body
+      const duplicateUser = await userService.findUserByEmail(newUser.email)
+
+      if (duplicateUser) {
+        return res.sendStatus(400)
+      }
+
+      const savedUser = await userService.register(newUser)
+
+      res.json(savedUser)
+    } catch (error) {
+      console.error(error)
+      return res.sendStatus(500)
+    }
+  }
+}
+
+export default new UserController()
